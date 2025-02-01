40ec949c74d1111297178373f8b1a2a65f56f2f5
Akanksha Adhav <adhavakanksha2103@gmail.com>
2025-02-01 07:16:25 +0530

    Initial commit

---
+++ urlShortner/src/main/java/com/example/urlShortner/Controller/UrlShorteningController.java
@@ -0,0 +1,74 @@
+package com.example.urlShortner.Controller;
+
+
+import com.example.urlShortner.model.Url;
+import com.example.urlShortner.model.UrlDto;
+import com.example.urlShortner.model.UrlErrorResponseDto;
+import com.example.urlShortner.model.UrlResponseDto;
+import com.example.urlShortner.service.UrlService;
+import jakarta.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@RestController
+public class UrlShorteningController {
+    @Autowired
+    private UrlService urlService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<?> generateShortLink(@RequestBody UrlDto urlDto){
+
+        Url urlToRet = urlService.generateShortLink(urlDto);
+        if(urlToRet != null){
+            UrlResponseDto urlResponseDto = new UrlResponseDto();
+            urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
+            urlResponseDto.setExpirationDate(urlToRet.getExpirationDate());
+            urlResponseDto.setShortLink(urlToRet.getShortLink());
+            return new ResponseEntity<UrlResponseDto>(urlResponseDto, HttpStatus.OK);
+        }
+
+        UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+        urlErrorResponseDto.setStatus("404");
+        urlErrorResponseDto.setError("There was an error");
+        return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto,HttpStatus.OK);
+    }
+
+    @GetMapping("/{shortLink}")
+    public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink) throws IOException {
+        if(StringUtils.isEmpty(shortLink)){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Invalid Url");
+            urlErrorResponseDto.setStatus("400");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto,HttpStatus.OK);
+        }
+
+        Url urlToRet = urlService.getEncodedUrl(shortLink);
+
+        if(urlToRet == null){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Url does not exist");
+            urlErrorResponseDto.setStatus("400");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto,HttpStatus.OK);
+        }
+
+        if(urlToRet.getExpirationDate().isBefore(LocalDateTime.now())){
+            UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+            urlErrorResponseDto.setError("Url Expired");
+            urlErrorResponseDto.setStatus("400");
+            return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto,HttpStatus.OK);
+        }
+
+        response.sendRedirect(urlToRet.getOriginalUrl());
+        return null;
+
+    }
+
+
+}
